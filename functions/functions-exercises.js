c7f731ef78795d8104cad46d1fa89ed2d821fa2b
JackNorkus <norkusjr@gmail.com>
2024-01-31 14:08:47 -0600

    added a comment for Doss

--- functions/functions-exercises.js
+++ functions/functions-exercises.js
@@ -1,4 +1,20 @@
-function makeLine(size, char) {
+/* Hey Doss, here is the code for the function exercises. I would not worry 
+about the char parameter until you get to the end of the exercise. Also,
+if your code is off by like 1 number or you have a newline somewhere where
+it shouldn't be, it's OKAY as long as it runs and does what you expect it to.
+Remember, if I have a variable, such as var, there are multiple ways of writing
+two different things. var++ is the same thing as var = var + 1, which is
+the same thing as var += 1. Programmers use these shorthands because they
+will come up often. I would recommend getting into the habit of trying to use
+these shorthands, simply because I think it will help you to make more sense
+of your code if you take a break from it. Lastly, when you are writing functions,
+it always helps to work backwards from the return statement. Think about what
+you want your function to return, code out the return statement, then write
+your actual function code. If you have any more questions I should be available
+most of tomorrow. Thanks and have a great day! - Jack
+*/
+
+function makeLine(size, char) { 
     char = char || '#'
     let numHashes = 0
     let line = "";
@@ -29,7 +45,6 @@ function makeRectangle(width, height, char) {
 
 function makeDownwardStairs(height, char) {
     char = char || '#';
-    let numRows = 0;
     let downwardStairs = "";
     for (let i = 1; i < height; i++) {
         downwardStairs += makeLine(i, char) + '\n';
