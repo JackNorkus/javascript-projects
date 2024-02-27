e7aab16e117625ea46a54c5139fd41919e0317b3
JackNorkus <norkusjr@gmail.com>
2024-02-26 19:22:10 -0600

    finished DOM and Events exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,25 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+
+    missionAbort.addEventListener('mouseover', event => {
+        missionAbort.style.backgroundColor = 'red';
+    });
+
+    missionAbort.addEventListener('mouseout', event => {
+        missionAbort.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener('click', event => {
+        let abortWindow = window.confirm("Are you sure you want to abort the mission?");
+        if (abortWindow) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+        }
+    });
     
 }
 
